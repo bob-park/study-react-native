cf4be014c9c41a38369acde55073cc6b0d1e7f86
Bob Park <hwpark11@kakao.com>
2025-05-25 14:59:37 +0900

    feat: tab menu 분리

--- src/app/(tabs)/(home)/index.tsx
+++ src/app/(tabs)/(home)/index.tsx
@@ -1,22 +1,23 @@
-import { useRouter } from 'expo-router';
+import TabMenu, { TabMenuItem } from '@/components/menu/TabMenu';
+
+import cx from 'classnames';
+import { usePathname, useRouter } from 'expo-router';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
   // hooks
   const router = useRouter();
+  const pathname = usePathname();
 
   return (
-    <View className="flex size-full flex-col items-center justify-center gap-2">
-      <View>
-        <TouchableOpacity onPress={() => router.push('/')}>
-          <Text>For you</Text>
-        </TouchableOpacity>
-      </View>
+    <View className="flex size-full flex-col items-center gap-2">
+      {/* header menu */}
 
-      <View>
-        <TouchableOpacity onPress={() => router.push('/following')}>
-          <Text>Following</Text>
-        </TouchableOpacity>
+      <View className="mt-24">
+        <TabMenu>
+          <TabMenuItem text="추천" active={pathname === '/'} onPress={() => router.push('/')} />
+          <TabMenuItem text="팔로잉" active={pathname === '/following'} onPress={() => router.push('/following')} />
+        </TabMenu>
       </View>
 
       <View>
---
+++ src/components/menu/TabMenu.tsx
@@ -0,0 +1,35 @@
+import cx from 'classnames';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+interface TabMenuProps {
+  children?: React.ReactNode;
+}
+
+export default function TabMenu({ children }: TabMenuProps) {
+  return <View className="flex w-full flex-row items-center justify-center">{children}</View>;
+}
+
+interface TabMenuItemProps {
+  active?: boolean;
+  text: string;
+  onPress?: () => void;
+}
+
+export function TabMenuItem({ active = false, text, onPress }: TabMenuItemProps) {
+  // handle
+  const handleClick = () => {
+    onPress && onPress();
+  };
+
+  return (
+    <View
+      className={cx('h-10 flex-1 border-b-2', {
+        'border-b-gray-300': !active,
+      })}
+    >
+      <TouchableOpacity className="w-full items-center" onPress={handleClick}>
+        <Text className={cx('text-xl', active ? 'font-bold' : 'text-gray-500')}>{text}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}

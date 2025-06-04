623092aa5b33e4d346ed70c8c192432c1627faa2
Bob Park <hwpark@malgn.com>
2025-06-04 18:28:38 +0900

    feat: appearance 기능 추가(~ing)

---
+++ src/shared/components/menu/SelectAppearanceMenu.tsx
@@ -0,0 +1,81 @@
+import { Appearance, Modal, SafeAreaView, Text, TouchableOpacity, View, useColorScheme } from 'react-native';
+
+import { BlurView } from 'expo-blur';
+
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+
+import cx from 'classnames';
+
+interface AppearanceMenuProps {
+  open: boolean;
+  onClose?: () => void;
+}
+
+export default function SelectAppearanceMenu({ open, onClose }: Readonly<AppearanceMenuProps>) {
+  // hooks
+  const colorScheme = useColorScheme();
+
+  // handle
+  const handleClose = () => {
+    onClose && onClose();
+  };
+
+  const handleChangeScheme = (scheme: 'dark' | 'light' | null) => {
+    Apearance.setColorScheme(scheme);
+  };
+
+  return (
+    <Modal visible={open} animationType="none" transparent onRequestClose={handleClose}>
+      <BlurView
+        className="relative flex flex-col items-center justify-center gap-2"
+        intensity={colorScheme === 'dark' ? 0 : 10}
+        tint="light"
+      >
+        {/* outside */}
+        <TouchableOpacity className="absolute left-0 top-0 h-screen w-screen" activeOpacity={1} onPress={handleClose} />
+
+        <SafeAreaView
+          className="absolute left-3 top-[90px] z-50 w-[60%] max-w-[280px] rounded-2xl bg-white"
+          style={{ shadowColor: '#000', shadowOpacity: 0.15, shadowOffset: { width: 2, height: 4 } }}
+        >
+          <View className="flex flex-col items-center justify-center gap-2 px-5 py-3">
+            <View className="relative flex w-full flex-row items-center justify-center gap-3">
+              <View className="items-center justify-center">
+                <Text className="text-lg font-semibold">Appearance</Text>
+              </View>
+
+              <TouchableOpacity className="absolute left-1 top-0" onPress={handleClose}>
+                <Ionicons name="arrow-back-outline" size={24} color="black" />
+              </TouchableOpacity>
+            </View>
+
+            <View className="mt-3 flex flex-row items-center justify-center gap-5">
+              <TouchableOpacity
+                className={cx(
+                  'size-10 items-center justify-center rounded-xl',
+                  colorScheme === 'light' && 'bg-gray-400',
+                )}
+                disabled={colorScheme === 'light'}
+              >
+                <MaterialIcons name="light-mode" size={24} color="black" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                className={cx(
+                  'size-10 items-center justify-center rounded-xl',
+                  colorScheme === 'dark' && 'bg-gray-400',
+                )}
+              >
+                <MaterialIcons name="dark-mode" size={24} color="black" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                className={cx('h-6 w-10 items-center justify-center rounded-xl', !colorScheme && 'bg-gray-500')}
+              >
+                <Text className="text-lg font-semibold">Auto</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </SafeAreaView>
+      </BlurView>
+    </Modal>
+  );
+}
--- src/shared/components/menu/SideMenu.tsx
+++ src/shared/components/menu/SideMenu.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import { Modal, SafeAreaView, Text, TouchableOpacity, View, useColorScheme } from 'react-native';
 
@@ -6,6 +6,7 @@ import { BlurView } from 'expo-blur';
 
 import { Ionicons } from '@expo/vector-icons';
 
+import SelectAppearanceMenu from '@/shared/components/menu/SelectAppearanceMenu';
 import { AuthContext } from '@/shared/providers/auth/AuthProvider';
 
 interface SideMenuProps {
@@ -19,6 +20,7 @@ export default function SideMenu({ open, onClose }: Readonly<SideMenuProps>) {
 
   // state
   const isLoggedIn = !!user;
+  const [showSelectAppearance, setShowSelectApperance] = useState<boolean>(false);
 
   // hooks
   const colorScheme = useColorScheme();
@@ -35,45 +37,55 @@ export default function SideMenu({ open, onClose }: Readonly<SideMenuProps>) {
   };
 
   return (
-    <Modal visible={open} animationType="fade" transparent onRequestClose={handleClose}>
-      <BlurView
-        className="relative flex flex-col items-center justify-center gap-2"
-        intensity={colorScheme === 'dark' ? 0 : 10}
-        tint="light"
-      >
-        {/* outside */}
-        <TouchableOpacity className="absolute left-0 top-0 h-screen w-screen" activeOpacity={1} onPress={handleClose} />
-
-        <SafeAreaView
-          className="absolute left-3 top-[90px] z-50 w-[70%] max-w-[320px] rounded-2xl bg-white"
-          style={{ shadowColor: '#000', shadowOpacity: 0.15, shadowOffset: { width: 2, height: 4 } }}
+    <>
+      <Modal visible={open && !showSelectAppearance} animationType="fade" transparent onRequestClose={handleClose}>
+        <BlurView
+          className="relative flex flex-col items-center justify-center gap-2"
+          intensity={colorScheme === 'dark' ? 0 : 10}
+          tint="light"
         >
-          <View className="">
-            <TouchableOpacity className="items-start justify-center px-5 py-3">
-              <Text className="text-lg font-semibold">Appearance</Text>
-              <Ionicons className="absolute right-5 top-4" name="chevron-forward" size={20} color="#888" />
-            </TouchableOpacity>
-            <TouchableOpacity className="items-start justify-center px-5 py-3">
-              <Text className="text-lg font-semibold">Insights</Text>
-            </TouchableOpacity>
-            <TouchableOpacity className="items-start justify-center px-5 py-3">
-              <Text className="text-lg font-semibold">Settings</Text>
-            </TouchableOpacity>
+          {/* outside */}
+          <TouchableOpacity
+            className="absolute left-0 top-0 h-screen w-screen"
+            activeOpacity={1}
+            onPress={handleClose}
+          />
 
-            <View className="h-[1px] w-full border-[0.5px] border-gray-400" />
-
-            <TouchableOpacity className="items-start justify-center px-5 py-3">
-              <Text className="text-lg font-semibold">Report a problem</Text>
-            </TouchableOpacity>
-
-            {isLoggedIn && (
-              <TouchableOpacity className="items-start justify-center px-5 py-3" onPress={handleLogout}>
-                <Text className="text-lg font-semibold text-red-600">Log out</Text>
+          <SafeAreaView
+            className="absolute left-3 top-[90px] z-50 w-[70%] max-w-[320px] rounded-2xl bg-white"
+            style={{ shadowColor: '#000', shadowOpacity: 0.15, shadowOffset: { width: 2, height: 4 } }}
+          >
+            <View className="">
+              <TouchableOpacity
+                className="items-start justify-center px-5 py-3"
+                onPress={() => setShowSelectApperance(true)}
+              >
+                <Text className="text-lg font-semibold">Appearance</Text>
+                <Ionicons className="absolute right-5 top-4" name="chevron-forward" size={20} color="#888" />
               </TouchableOpacity>
-            )}
-          </View>
-        </SafeAreaView>
-      </BlurView>
-    </Modal>
+              <TouchableOpacity className="items-start justify-center px-5 py-3">
+                <Text className="text-lg font-semibold">Insights</Text>
+              </TouchableOpacity>
+              <TouchableOpacity className="items-start justify-center px-5 py-3">
+                <Text className="text-lg font-semibold">Settings</Text>
+              </TouchableOpacity>
+
+              <View className="h-[1px] w-full border-[0.5px] border-gray-400" />
+
+              <TouchableOpacity className="items-start justify-center px-5 py-3">
+                <Text className="text-lg font-semibold">Report a problem</Text>
+              </TouchableOpacity>
+
+              {isLoggedIn && (
+                <TouchableOpacity className="items-start justify-center px-5 py-3" onPress={handleLogout}>
+                  <Text className="text-lg font-semibold text-red-600">Log out</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+          </SafeAreaView>
+        </BlurView>
+      </Modal>
+      <SelectAppearanceMenu open={showSelectAppearance} onClose={() => setShowSelectApperance(false)} />
+    </>
   );
 }

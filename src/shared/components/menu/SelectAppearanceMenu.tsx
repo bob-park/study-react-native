b86c293a2018eb128996711da1db42c44a9b1923
Bob Park <hwpark11@kakao.com>
2025-06-05 01:44:43 +0900

    feat: 아이콘 변경

--- src/shared/components/menu/SelectAppearanceMenu.tsx
+++ src/shared/components/menu/SelectAppearanceMenu.tsx
@@ -4,7 +4,7 @@ import { Modal, SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
 
 import { BlurView } from 'expo-blur';
 
-import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { Ionicons, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
 import { ThemeContext } from '@/shared/providers/theme/ThemeProvider';
 
@@ -39,16 +39,16 @@ export default function SelectAppearanceMenu({ open, onClose }: Readonly<Appeara
         <TouchableOpacity className="absolute left-0 top-0 h-screen w-screen" activeOpacity={1} onPress={handleClose} />
 
         <SafeAreaView
-          className="absolute left-3 top-[90px] z-50 w-[60%] max-w-[280px] rounded-2xl border-[1px] border-gray-300 bg-white dark:bg-black"
+          className="absolute left-3 top-[90px] z-50 w-[50%] rounded-2xl border-[1px] border-gray-300 bg-white dark:bg-black"
           style={{ shadowColor: '#000', shadowOpacity: 0.15, shadowOffset: { width: 2, height: 4 } }}
         >
-          <View className="flex flex-col items-center justify-center gap-2 px-5 py-3">
+          <View className="flex w-full flex-col items-center justify-center gap-2 p-3">
             <View className="relative flex w-full flex-row items-center justify-center gap-3">
               <View className="items-center justify-center">
                 <Text className="text-lg font-semibold dark:text-white">Appearance</Text>
               </View>
 
-              <TouchableOpacity className="absolute left-1 top-0" onPress={handleClose}>
+              <TouchableOpacity className="absolute left-0 top-0" onPress={handleClose}>
                 <Ionicons name="arrow-back-outline" size={24} color={theme === 'light' ? 'black' : 'white'} />
               </TouchableOpacity>
             </View>
@@ -81,19 +81,17 @@ export default function SelectAppearanceMenu({ open, onClose }: Readonly<Appeara
               </TouchableOpacity>
               <TouchableOpacity
                 className={cx(
-                  'h-10 w-16 items-center justify-center rounded-xl',
+                  'size-10 items-center justify-center rounded-xl',
                   preference === 'system' && 'bg-gray-300',
                 )}
                 disabled={preference === 'system'}
                 onPress={() => handleChangeScheme('system')}
               >
-                <Text
-                  className={cx('text-lg font-semibold', {
-                    'dark:text-white': preference !== 'system',
-                  })}
-                >
-                  Auto
-                </Text>
+                <MaterialCommunityIcons
+                  name="desktop-mac"
+                  size={24}
+                  color={theme === 'light' || preference === 'system' ? 'black' : 'white'}
+                />
               </TouchableOpacity>
             </View>
           </View>

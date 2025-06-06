4deef2048c9303c59b5bb925478e55d40f4dcfad
Bob Park <hwpark11@kakao.com>
2025-06-07 02:51:33 +0900

    feat: mock pattern 추가

--- src/index.ts
+++ src/index.ts
@@ -9,29 +9,32 @@ declare global {
 }
 
 if (__DEV__) {
-  // if (window.server) {
-  //   window.server.shutdown();
-  // }
-  // window.server = createServer({
-  //   routes() {
-  //     this.namespace = 'api';
-  //
-  //     this.post('/login', (schema, request) => {
-  //       const { username, password } = JSON.parse(request.requestBody);
-  //
-  //       if (username === 'bobpark' && password === '12345') {
-  //         return {
-  //           accessToken: 'access-token',
-  //           refreshToken: 'refresh-token',
-  //           user: {
-  //             id: 'bob-park',
-  //             username: '박현우',
-  //           },
-  //         };
-  //       } else {
-  //         return new Response(401, {}, { message: 'Invaild credentials' });
-  //       }
-  //     });
-  //   },
-  // });
+  if (window.server) {
+    window.server.shutdown();
+  }
+
+  window.server = createServer({
+    routes() {
+      this.passthrough((req) => req.url.startsWith(`${process.env.EXPO_PUBLIC_AUTHORIZATION_SERVER}`));
+
+      this.namespace = 'api';
+
+      this.post('/login', (schema, request) => {
+        const { username, password } = JSON.parse(request.requestBody);
+
+        if (username === 'bobpark' && password === '12345') {
+          return {
+            accessToken: 'access-token',
+            refreshToken: 'refresh-token',
+            user: {
+              id: 'bob-park',
+              username: '박현우',
+            },
+          };
+        } else {
+          return new Response(401, {}, { message: 'Invaild credentials' });
+        }
+      });
+    },
+  });
 }
